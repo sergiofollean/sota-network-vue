11fb977a11d4c0b01977098246d124aec45c71d5
Sergey Popovich <sergey.po@thewhitelabelagency.com>
2021-10-06 18:50:03 +0300

    Firebase initialized

--- src/main.js
+++ src/main.js
@@ -18,6 +18,10 @@ import App from "./App.vue";
 import router from "./router";
 import store from "./store";
 
+import firebase from 'firebase/app';
+import 'firebase/auth';
+import 'firebase/firestore';
+
 import vuetify from "./plugins/vuetify";
 import "./plugins";
 import { makeServer } from "../server";
@@ -42,5 +46,24 @@ new Vue({
   store,
   router,
   vuetify,
-  render: h => h(App)
+  render: h => h(App),
+  created () {
+    if(!firebase.apps.length) {
+      const firebaseConfig = {
+        apiKey: "AIzaSyA2FX8u7Q_rgx5gi32730BEyrAxfD53DJ8",
+        authDomain: "botapp-bb4b0.firebaseapp.com",
+        databaseURL: "https://botapp-bb4b0-default-rtdb.firebaseio.com",
+        projectId: "botapp-bb4b0",
+        storageBucket: "botapp-bb4b0.appspot.com",
+        messagingSenderId: "245702413811",
+        appId: "1:245702413811:web:5d370f49db6db0a6f41686",
+        measurementId: "G-PL69VSBB8X"
+      };
+
+      firebase.initializeApp(firebaseConfig);
+    }
+    else {
+      firebase.app();
+    }
+  }
 }).$mount("#app");
