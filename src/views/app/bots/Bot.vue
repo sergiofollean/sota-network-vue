545954770db2c20ed5af999e9079c279778f7910
Sergey Popovich <sergiofollean@gmail.com>
2021-10-14 20:58:19 +0300

    Bot: select account and market done

--- src/views/app/bots/Bot.vue
+++ src/views/app/bots/Bot.vue
@@ -18,13 +18,17 @@
                 <v-col sm="6">
                   <v-select
                       outlined
-                      label="Маркет"
+                      label="Акаунт"
+                      :items="priceDrivers"
+                      @change="test"
                   />
                 </v-col>
                 <v-col sm="6">
                   <v-select
                       outlined
-                      label="Акаунт"
+                      label="Маркет"
+                      no-data-text="Спершу оберіть акаунт"
+                      :items="markets"
                   />
                 </v-col>
               </v-row>
@@ -44,6 +48,7 @@
               <v-text-field
                   outlined
                   label="Баланс для торгівлі"
+                  prepend-inner-icon="mdi-currency-usd"
               />
             </v-card-text>
             <v-card-text>
@@ -58,17 +63,60 @@
 
 <script>
 import axios from "axios";
+import firebase from "firebase";
 
 export default {
   data() {
     return {
-      level: [ "Низький ризик", "Середній ризик", "Високий ризик" ]
+      level: [ "Низький ризик", "Середній ризик", "Високий ризик" ],
+      priceDrivers: [],
+      markets: []
     }
   },
   created() {
-    axios.get('http://34.116.142.95/GetPriceMarkets').then(response => {
-      console.log(response);
+    var db = firebase.firestore();
+    firebase.auth().onAuthStateChanged(async user => {
+      var priceDrivers = db.collection('users').doc(user.uid).collection('PriceDrivers');
+      priceDrivers.onSnapshot(snapshot => {
+        this.priceDrivers = [];
+        snapshot.forEach(doc => {
+          this.priceDrivers.push({
+            text: doc.data()['AccountName'],
+            value: doc.id,
+            type: doc.data()['AccountType'],
+            platform: doc.data()['AccountPlatform']
+          });
+        });
+      });
     });
+    // axios.get('http://34.116.142.95/GetPriceMarkets').then(response => {
+    //   console.log(response);
+    // });
+  },
+  methods: {
+    test(e) {
+      var priceDriver = this.priceDrivers.find(obj => {
+        return obj.value === e
+      });
+
+      var platform = '';
+      // Binance
+      if(priceDriver.platform === 'Binance') {
+        if(priceDriver.type === 'Leverage Trading') {
+          platform = 'BinanceFutures'
+        }
+      }
+
+      if(platform.length > 0) {
+        axios.get('http://34.116.142.95/GetPriceMarkets/'+platform).then(response => {
+          if(response.data.length > 0) {
+            response.data.map(el => {
+              this.markets.push({ text: el.ShortName, value: el.DisplayName });
+            });
+          }
+        });
+      }
+    }
   }
 }
 </script>
