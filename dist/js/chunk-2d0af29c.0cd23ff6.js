0017e258a0f8dab63714b7b69ae3ef371dcbf1fc
Sergey Popovich <sergiofollean@gmail.com>
2021-11-07 16:44:40 +0200

    Deploy v1.5 fix

--- dist/js/chunk-2d0af29c.0cd23ff6.js
+++
@@ -1,6 +0,0 @@
-(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["chunk-2d0af29c"],{"0cc6":function(n,c,e){"use strict";e.r(c);var t=function(){var n=this,c=n.$createElement,e=n._self._c||c;return e("div")},u=[],l=e("2877"),a={},r=Object(l["a"])(a,t,u,!1,null,null,null);c["default"]=r.exports}}]);
-<<<<<<< HEAD:dist/js/chunk-2d0af29c.0cd23ff6.js
-//# sourceMappingURL=chunk-2d0af29c.0cd23ff6.js.map
-=======
-//# sourceMappingURL=chunk-2d0af29c.9b86adb6.js.map
->>>>>>> 75b02941a75d0cafa7b6fdde8ea18c888ed20119:dist/js/chunk-2d0af29c.9b86adb6.js
--- dist/js/chunk-2d2311c7.95985da2.js
+++
@@ -1,6 +0,0 @@
-(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["chunk-2d2311c7"],{eec4:function(e,n,c){"use strict";c.r(n);var t=function(){var e=this,n=e.$createElement,c=e._self._c||n;return c("div")},u=[],l=c("2877"),r={},s=Object(l["a"])(r,t,u,!1,null,null,null);n["default"]=s.exports}}]);
-<<<<<<< HEAD:dist/js/chunk-2d2311c7.c3a687fa.js
-//# sourceMappingURL=chunk-2d2311c7.c3a687fa.js.map
-=======
-//# sourceMappingURL=chunk-2d2311c7.95985da2.js.map
->>>>>>> 75b02941a75d0cafa7b6fdde8ea18c888ed20119:dist/js/chunk-2d2311c7.95985da2.js
