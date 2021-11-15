a6a1ae856a741afcd1a45983918a6be9991d8399
Vasyl Honcharenko <vasilijgoncarenko4@gmail.com>
2021-11-15 22:24:08 +0200

    remove dist.

--- dist/js/chunk-2d0c4302.e1e7a9b1.js
+++
@@ -1,2 +0,0 @@
-(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["chunk-2d0c4302"],{"3a86":function(e,t,n){"use strict";n.r(t);var s=function(){var e=this,t=e.$createElement,n=e._self._c||t;return n("router-view")},u=[],a={name:"settings",metaInfo:{title:"Settings"}},c=a,i=n("2877"),l=Object(i["a"])(c,s,u,!1,null,null,null);t["default"]=l.exports}}]);
-//# sourceMappingURL=chunk-2d0c4302.e1e7a9b1.js.map
