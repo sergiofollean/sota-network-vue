dd3bf01923d2e414220396121ebf5cc427c1cdeb
Vasyl Honcharenko <vasilijgoncarenko4@gmail.com>
2021-12-14 03:46:46 +0200

    remove local dist.

--- dist/js/chunk-2d0af29c.9c744354.js
+++
@@ -1,2 +0,0 @@
-(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["chunk-2d0af29c"],{"0cc6":function(n,c,e){"use strict";e.r(c);var t=function(){var n=this,c=n.$createElement,e=n._self._c||c;return e("div")},u=[],l=e("2877"),a={},r=Object(l["a"])(a,t,u,!1,null,null,null);c["default"]=r.exports}}]);
-//# sourceMappingURL=chunk-2d0af29c.9c744354.js.map
